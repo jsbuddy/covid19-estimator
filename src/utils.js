2eee0ab0cab8a29220ad00d2b25929bf34b6b938
jsbuddy <judecodes@gmail.com>
2020-04-14 13:17:54 +0100

    create OBJtoXML utility function

--- src/utils.js
+++ src/utils.js
@@ -33,3 +33,33 @@ export const computeHospitalBedsByRequestedTime = (
   totalHospitalBeds,
   severeCasesByRequestedTime
 ) => Math.trunc(((35 / 100) * totalHospitalBeds) - severeCasesByRequestedTime);
+
+export const OBJtoXML = (obj, root = true) => {
+  let xml = '';
+  if (root) {
+    xml += '<?xml version="1.0" encoding="UTF-8" ?>';
+    xml += '<root>';
+  }
+  // eslint-disable-next-line guard-for-in, no-restricted-syntax
+  for (const prop in obj) {
+    xml += obj[prop] instanceof Array ? '' : `<${prop}>`;
+    if (obj[prop] instanceof Array) {
+      // eslint-disable-next-line guard-for-in, no-restricted-syntax
+      for (const array in obj[prop]) {
+        xml += `<${prop}>`;
+        xml += OBJtoXML({ ...obj[prop][array] }, false);
+        xml += `</${prop}>`;
+      }
+    } else if (typeof obj[prop] === 'object') {
+      xml += OBJtoXML({ ...obj[prop] }, false);
+    } else {
+      xml += obj[prop];
+    }
+    xml += obj[prop] instanceof Array ? '' : `</${prop}>`;
+  }
+  if (root) {
+    xml += '</root>';
+  }
+  xml = xml.replace(/<\/?[0-9]{1,}>/g, '');
+  return xml;
+};
